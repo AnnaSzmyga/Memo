0ff6f8bdc0db77671c7e30da3dd799c9a016f4b0
Anna Szmyga <anna.szmyga@o2.pl>
2019-11-20 15:35:50 +0100

    Style winners modal

--- src/components/WinnersList/WinnersList.js
+++ src/components/WinnersList/WinnersList.js
@@ -6,22 +6,24 @@ import './WinnersList.scss';
 
 const WinnersList = ({ winners, showWinners, closeWinnersModal }) => {
     return (
-        <Modal className="winners" isOpen={showWinners} fade={true}>
-            <h2 className="winners__heading">Memo winners</h2>
-            <table className="winners__table">
-                <thead>
-                    <tr>
-                        <th>Player</th>
-                        <th>Time</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        winners.map((winner, i) => <Winner key={i} winner={winner} />)
-                    }
-                </tbody>
-            </table>
-            <button className="winners__close-btn" onClick={closeWinnersModal}>X</button>
+        <Modal isOpen={showWinners} fade={true}>
+            <div className="winners">
+                <h2 className="winners__heading">Memo winners</h2>
+                <table className="winners__table">
+                    <thead>
+                        <tr>
+                            <th>Player:</th>
+                            <th>Time:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            winners.map((winner, i) => <Winner key={i} winner={winner} />)
+                        }
+                    </tbody>
+                </table>
+                <button className="winners__close-btn" onClick={closeWinnersModal}>x</button>
+            </div>
         </Modal>
     )
 }
